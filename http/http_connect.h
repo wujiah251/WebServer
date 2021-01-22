ea9774f609b49d0b285bd161e5f0184d6cef041b
wujiah251 <wujiah251>
2021-01-22 15:17:53 +0800

    http连接类头文件实现

--- http/http_connect.h
+++ http/http_connect.h
@@ -1,8 +1,148 @@
 #ifndef WEBSERVER_HTTP_HTTP_CONNECT_H
 #define WEBSERVER_HTTP_HTTP_CONNECT_H
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/uio.h>
+#include <map>
+
+#include "../lock/locker.h"
+#include "../CGImysql/sql_connection_pool.h"
+#include "../timer/lst_timer.h"
+#include "../log/log.h"
 
 class Http_connect
 {
+public:
+    static const int FILENAME_LEN = 200;
+    static const int READ_BUFFER_SIZE = 2048;
+    static const int WRITE_BUFFER_SIZE = 1024;
+    enum METHOD
+    {
+        GET = 0,
+        POST,
+        HEAD,
+        PUT,
+        DELETE,
+        TRACE,
+        OPTIONS,
+        CONNECT,
+        PATH
+    };
+    enum CHECK_STATE
+    {
+        CHECK_STATE_REQUESTLINE = 0,
+        CHECK_STATE_HEADER,
+        CHECK_STATE_CONTENT
+    };
+    enum HTTP_CODE
+    {
+        NO_REQUEST,
+        GET_REQUEST,
+        BAD_REQUEST,
+        NO_RESOURCE,
+        FORBIDDEN_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION
+    };
+    enum LINE_STATUS
+    {
+        LINE_OK = 0,
+        LINE_BAD,
+        LINE_OPEN
+    };
+
+public:
+    void init(int socket_fd, const sockaddr_in &addr, char *, int, int, string user, string user, string password, sqlname);
+    void close_connect(bool read_close = true);
+    void process();
+    bool read_once();
+    bool write();
+    sockaddr_in *get_address()
+    {
+        return &address_;
+    }
+    void init_mysql_result(Connection_pool *connectPool);
+    int timer_flag_;
+    int improve_;
+
+private:
+    void init();
+    HTTP_CODE process_read();
+    bool process_write(HTTP_CODE ret);
+    HTTP_CODE parse_request_line(char *text);
+    HTTP_CODE parse_headers(char *text);
+    HTTP_CODE do_request();
+    char *get_line()
+    {
+        return read_buf_ + start_line_;
+    }
+    LINE_STATUS parse_line();
+    void unmap();
+    bool add_response(const char *format, ...);
+    bool add_content(const char *content);
+    bool add_status_line(int status, const char *title);
+    bool add_headers(int content_length);
+    bool add_content_type();
+    bool add_content_length(int content_length);
+    bool add_linger();
+    bool add_blank_line();
+
+public:
+    static int epoll_fd_;
+    static int user_count_;
+    MYSQL *mysql;
+    int state_; //读为0，写为1
+private:
+    int socket_fd_;
+    sockaddr_in address_;
+    char read_buf_[READ_BUFFER_SIZE];
+    int read_idx_;
+    int checked_idx_;
+    int start_line_;
+    char write_buf_[WRITE_BUFFER_SIZE];
+    int write_idx_;
+    CHECK_STATE check_state_;
+    METHOD method_;
+    char read_file_[FILENAME_LEN];
+    char *url_;
+    char *version_;
+    char *host_;
+    char content_length_;
+    bool linger_;
+    char *file_address_;
+    struct stat file_stat_;
+    struct iovec iv_[2];
+    int iv_count_;
+    int cgi;       //是否启用POST
+    char *string_; //存储请求头数据
+    int bytes_to_send_;
+    int bytes_have_send_;
+    char *doc_root_;
+
+    map<string, string> users_;
+    int trig_mode_;
+    int close_log_;
+
+    char sql_user_[100];
+    char sql_password_[100];
+    char sql_name_[100];
 };
 
-#endif WEBSERVER_HTTP_HTTP_CONNECT_H
+#endif
