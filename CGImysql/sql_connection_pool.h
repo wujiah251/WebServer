89cd6bc2404c128bad0853691c439d01f09180ca
wujiah251 <wujiah251>
2021-01-24 16:53:56 +0800

    数据库连接池修改

--- CGImysql/sql_connection_pool.h
+++ CGImysql/sql_connection_pool.h
@@ -1,5 +1,5 @@
-#ifndef WEBSERVER_CGIMYSQL_SQL_CONNECTION_POOL_H_
-#define WEBSERVER_CGIMYSQL_SQL_CONNECTION_POOL_H_
+#ifndef _CONNECTION_POOL_
+#define _CONNECTION_POOL_
 
 #include <stdio.h>
 #include <list>
@@ -13,47 +13,49 @@
 
 using namespace std;
 
-class Connection_pool
+class connection_pool
 {
 public:
-    MYSQL *get_connection();                 //获取数据库连接
-    bool release_connection(MYSQL *connect); //释放连接
-    int get_free_connection();               //获取连接
-    void destroy_pool();                     //销毁所有连接
+	MYSQL *GetConnection();				 //获取数据库连接
+	bool ReleaseConnection(MYSQL *conn); //释放连接
+	int GetFreeConn();					 //获取连接
+	void DestroyPool();					 //销毁所有连接
 
-    //单例模式
-    static Connection_pool *get_instance();
-    void init(string url, string user, string password, string database_name, int port, int max_connection, int close_log);
+	//单例模式
+	static connection_pool *GetInstance();
+
+	void init(string url, string User, string PassWord, string DataBaseName, int Port, int MaxConn, int close_log);
 
 private:
-    Connection_pool();
-    ~Connection_pool();
+	connection_pool();
+	~connection_pool();
 
-    int max_connection_;         //最大连接数
-    int cur_connection_;         //当前已经使用的连接数
-    int free_connection_;        //当前空闲的连接数
-    Locker lock_;                //互斥锁
-    list<MYSQL *> connect_list_; //连接池
-    Sem reserve_;                //信号量，用来表示可用的空闲连接数
+	int m_MaxConn;	//最大连接数
+	int m_CurConn;	//当前已使用的连接数
+	int m_FreeConn; //当前空闲的连接数
+	locker lock;
+	list<MYSQL *> connList; //连接池
+	sem reserve;
 
 public:
-    string url_;           //主机地址
-    string port_;          //数据库端口号
-    string user_;          //登陆数据库用户名
-    string password_;      //登陆数据库密码
-    string database_name_; //使用数据库名
-    int close_log_;        //日志开关
+	string m_url;		   //主机地址
+	string m_Port;		   //数据库端口号
+	string m_User;		   //登陆数据库用户名
+	string m_PassWord;	   //登陆数据库密码
+	string m_DatabaseName; //使用数据库名
+	int m_close_log;	   //日志开关
 };
 
-class ConnectionRAII
+class connectionRAII
 {
+
 public:
-    ConnectionRAII(MYSQL **connect, Connection_pool *connect_pool);
-    ~ConnectionRAII();
+	connectionRAII(MYSQL **con, connection_pool *connPool);
+	~connectionRAII();
 
 private:
-    MYSQL *connectRAII_;
-    Connection_pool *poolRAII_;
+	MYSQL *conRAII;
+	connection_pool *poolRAII;
 };
 
 #endif
