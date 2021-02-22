b53ee0e21c8757d364a795988d63a6f83c59917f
wujiahao <2121866416@qq.com>
2021-02-22 22:10:14 +0800

    添加框架设计图片

--- include/http/http_connect.cc
+++ include/http/http_connect.cc
@@ -454,22 +454,6 @@ http_conn::HTTP_CODE http_conn::do_request()
 
         free(m_url_real);
     }
-    else if (*(p + 1) == '6')
-    {
-        char *m_url_real = (char *)malloc(sizeof(char) * 200);
-        strcpy(m_url_real, "/video.html");
-        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
-
-        free(m_url_real);
-    }
-    else if (*(p + 1) == '7')
-    {
-        char *m_url_real = (char *)malloc(sizeof(char) * 200);
-        strcpy(m_url_real, "/fans.html");
-        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
-
-        free(m_url_real);
-    }
     else
         strncpy(m_real_file + len, m_url, FILENAME_LEN - len - 1);
 
